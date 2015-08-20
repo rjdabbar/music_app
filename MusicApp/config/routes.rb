22d23a330b31ac89249e8166167c846168a5979f
RJ Dabbar <rjdabbar@gmail.com>
2015-08-20 11:27:26 -0400

    Add routes for bands, albums, tracks.

--- MusicApp/config/routes.rb
+++ MusicApp/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+
+  resources :bands do
+    resources :albums, only: [:new]
+  end
+
+  resources :albums, only: [:create, :update, :edit, :show, :destroy] do
+    resources :tracks, only: [:new]
+  end
+  resources :tracks, only: [:create, :update, :edit, :show, :destroy]
+
+
 end

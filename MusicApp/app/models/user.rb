e1f53cf45cdcfd8a7f548b02227d93cb4de3ee14
RJ Dabbar <rjdabbar@gmail.com>
2015-08-20 10:20:33 -0400

    User model methods added for session tracking and password storing.

--- MusicApp/app/models/user.rb
+++ MusicApp/app/models/user.rb
@@ -5,8 +5,40 @@ class User < ActiveRecord::Base
   validates :email, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true}
 
+  after_initialize :ensure_session_token
+
   def self.generate_session_token
     SecureRandom::urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+    user.is_password?(password) ? user : nil
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def password_digest
+    BCrypt::Password.new(super)
+  end
+
+  def is_password?(password)
+    self.password_digest.is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
   end
 
 end
